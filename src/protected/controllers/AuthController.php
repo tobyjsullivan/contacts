49ce3d35d803650b55cb5057be06693571afa241
Toby Sullivan <tobyjsullivan@gmail.com>
2013-09-30 15:31:48 -0700

    Added error handler to authcontroller

--- src/protected/controllers/AuthController.php
+++ src/protected/controllers/AuthController.php
@@ -68,4 +68,18 @@ class AuthController extends Controller {
 		
 		$this->redirect(array('/site/index'));
 	}
+
+	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionError()
+	{
+		if($error=Yii::app()->errorHandler->error)
+		{
+			if(Yii::app()->request->isAjaxRequest)
+				echo $error['message'];
+			else
+				$this->render('error', $error);
+		}
+	}
 }
