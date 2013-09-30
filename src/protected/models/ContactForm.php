9b4e2d3fa426ae7e4700de7d0db39c8bca6c5065
Toby Sullivan <tobyjsullivan@gmail.com>
2013-09-30 16:14:03 -0700

    Added form input length enforcement to match DB limits

--- src/protected/models/ContactForm.php
+++ src/protected/models/ContactForm.php
@@ -13,6 +13,9 @@ class ContactForm extends CFormModel {
 	{
 		return array(
 				array('name', 'required'),
+				array('name', 'length', 'min'=>1, 'max'=>42),
+				array('phone', 'length', 'max'=>20),
+				array('twitter', 'length', 'max'=>17),
 		);
 	}
 }
