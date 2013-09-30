6e0fa929bd87b9f845088941f4f7f8c8f1f347bc
Toby Sullivan <tobyjsullivan@gmail.com>
2013-09-29 17:04:11 -0700

    Implemented a couple more model tests

--- src/protected/tests/unit/ContactTest.php
+++ src/protected/tests/unit/ContactTest.php
@@ -7,10 +7,9 @@ class ContactTest extends CDbTestCase {
 		);
 	
 	public function testCreateNewWithNameOnly() {
-		
+		$owner_id = $this->users['sample1']['user_id'];
 		
 		$contact = new Contact;
-		$owner_id = $this->users['sample1']['user_id'];
 		$contact->owner_id = $owner_id;
 		$contact->name = "Eunice Dias";
 		$contact->save();
@@ -23,5 +22,27 @@ class ContactTest extends CDbTestCase {
 		$foundContact = Contact::model()->find('contact_id=:contact_id', array(':contact_id'=>$contact_id));
 		
 		$this->assertEquals("Eunice Dias", $foundContact->name);
+	}
+	
+	public function testCreateNewWithAllInfo() {
+		$owner_id = $this->users['sample1']['user_id'];
+
+		$contact = new Contact;
+		$contact->owner_id = $owner_id;
+		$contact->name = "Howard Osborn";
+		$contact->phone = "215-853-0456";
+		$contact->twitter = "hosborn";
+		$contact->save();
+		
+		$contact_id = $contact->contact_id;
+		
+		$this->assertNotEquals("0", $contact_id);
+		
+		$contact = null;
+		$foundContact = Contact::model()->find('contact_id=:contact_id', array(':contact_id'=>$contact_id));
+
+		$this->assertEquals("Howard Osborn", $foundContact->name);
+		$this->assertEquals("215-853-0456", $foundContact->phone);
+		$this->assertEquals("hosborn", $foundContact->twitter);
 	}
 }
--- src/protected/tests/unit/UserTest.php
+++ src/protected/tests/unit/UserTest.php
@@ -9,5 +9,11 @@ class UserTest extends CDbTestCase {
 		$user_id = $user->user_id;
 		
 		$this->assertNotEquals("0", $user_id);
+		
+		// Find the user by twitter id
+		$foundUser = User::model()->find('twitter_id=:twitter_id', array(':twitter_id'=>"827410"));
+		
+		// Confirm same user id is found
+		$this->assertEquals($user_id, $foundUser->user_id);
 	}
 }
