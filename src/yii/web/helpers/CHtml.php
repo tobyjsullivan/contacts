26abab18905e0aab0ff6818784cd7d6bc3482b9a
Toby Sullivan <tobyjsullivan@gmail.com>
2013-10-01 16:42:52 -0700

    Adjusted error wording

--- src/yii/web/helpers/CHtml.php
+++ src/yii/web/helpers/CHtml.php
@@ -2013,7 +2013,7 @@ EOD;
 		if($content!=='')
 		{
 			if($header===null)
-				$header='<p>'.Yii::t('yii','Please fix the following input errors:').'</p>';
+				$header='<p>'.Yii::t('yii','Hey, we\'re almost good. Just help with this:').'</p>';
 			if(!isset($htmlOptions['class']))
 				$htmlOptions['class']=self::$errorSummaryCss;
 			return self::tag('div',$htmlOptions,$header."\n<ul>\n$content</ul>".$footer);
