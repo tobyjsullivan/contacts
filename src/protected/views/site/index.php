818978db87071b04b196a94f43dbeb70327c6164
Toby Sullivan <tobyjsullivan@gmail.com>
2013-10-01 00:36:03 -0700

    Switched to proper Sign in with Twitter button

--- src/protected/views/site/index.php
+++ src/protected/views/site/index.php
@@ -7,5 +7,10 @@ $this->showNav = false;
 ?>
 
 <div class="text-center">
-	<p><?php echo CHtml::link('Sign in with Twitter', array('/auth/SignInWithTwitter'), array('class' => 'btn btn-primary')); ?></p>
+	<p>Welcome to your new contact list. It's super handy!</p>
+	<p><?php 
+	$image_url = Yii::app()->params['assetHost'] . "/images/sign-in-with-twitter-gray.png";
+	$image_tag = CHtml::image($image_url, 'Sign in with Twitter');
+	echo CHtml::link($image_tag, array('/auth/SignInWithTwitter'));
+	 ?></p>
 </div>
